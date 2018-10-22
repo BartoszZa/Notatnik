549b6b814afbcec7b671985a96e4538899cfcdef
BartoszZa <44377919+BartoszZa@users.noreply.github.com>
2018-10-22 21:33:31 +0200

    Add Tests

---
+++ NUnit.Tests.Droid1/MockResults/GetTask.cs
@@ -0,0 +1,21 @@
+using NOTATNIK.Models.Database;
+using NOTATNIK.Models.Interfaces;
+
+namespace NUnit.Tests.Droid1.MockResults
+{
+    public class GetTask : IGetTask
+    {
+        public Task Task { get; set; }
+
+        public async System.Threading.Tasks.Task DatabaseCall(int id)
+        {
+            Task taskDB = new Task()
+            {
+                  Id = 1,
+                  Subject = "test",
+                  Content = "test"
+            };
+            Task = taskDB;
+        }
+    }
+}
---
+++ NUnit.Tests.Droid1/TestClass.cs
@@ -0,0 +1,39 @@
+﻿using NOTATNIK.ViewModels;
+using NUnit.Framework;
+
+namespace NUnit.Tests.Droid1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestId()
+        {
+            AddTaskViewModel AddTaskVM = new AddTaskViewModel();
+            int id = 1;
+            GetTask getTask = new GetTask();
+            AddTaskVM.Load(getTask, id);
+            Assert.AreEqual(1, getTask.Task.Id);
+        }
+
+        [Test]
+        public void TestSubject()
+        {
+            AddTaskViewModel AddTaskVM = new AddTaskViewModel();
+            int id = 1;
+            GetTask getTask = new GetTask();
+            AddTaskVM.Load(getTask, id);
+            Assert.AreEqual("test", getTask.Task.Subject);
+        }
+
+        [Test]
+        public void TestContent()
+        {
+            AddTaskViewModel AddTaskVM = new AddTaskViewModel();
+            int id = 1;
+            GetTask getTask = new GetTask();
+            AddTaskVM.Load(getTask, id);
+            Assert.AreEqual("test", getTask.Task.Content);
+        }
+    }
+}
