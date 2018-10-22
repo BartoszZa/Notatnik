6f5bb811f3435445c6b5e4cc842455ec809b2870
BartoszZa <44377919+BartoszZa@users.noreply.github.com>
2018-10-22 21:18:45 +0200

    Initialization

---
+++ NOTATNIK/NOTATNIK/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using NOTATNIK.Views;
+
+using Xamarin.Forms;
+
+namespace NOTATNIK
+{
+    /// <summary> 
+    /// Klasa startowa projektu.
+    /// </summary> 
+    public partial class App : Application
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        /// <summary> 
+        /// Konstruktor.
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new InitializationPage());
+        }
+    }
+}
