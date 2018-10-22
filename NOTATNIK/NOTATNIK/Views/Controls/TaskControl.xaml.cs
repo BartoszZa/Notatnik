69fc87950daf5713108c69b276568d2d7fe9137e
BartoszZa <44377919+BartoszZa@users.noreply.github.com>
2018-10-22 21:30:02 +0200

    Add Controls in Views

---
+++ NOTATNIK/NOTATNIK/Views/Controls/TaskControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NOTATNIK.Views.Controls
+{
+    public partial class TaskControl : ContentView
+    {
+        public TaskControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
